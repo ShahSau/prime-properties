c3358f6c4dff10e4b8424ea7358efa8d568d6081
shahriar <shahriarksaurov@gmail.com>
2023-10-21 16:59:02 +0300

    show and delete listing

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { updateUser, deleteUser } from '../controllers/user.controller.js'
+import { updateUser, deleteUser, getUserListings } from '../controllers/user.controller.js'
 import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router()
@@ -11,5 +11,6 @@ router.get('/test', (req, res) => {
 router.post('/update/:id', verifyToken, updateUser)
 
 router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
 
 export default router
