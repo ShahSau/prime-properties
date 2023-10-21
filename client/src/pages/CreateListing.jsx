a135c50da98cbd53c4455b534bce44ba640928b1
shahriar <shahriarksaurov@gmail.com>
2023-10-21 21:20:55 +0300

    listing page (WIP)

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -309,7 +309,9 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
             {/* <div className='flex items-center gap-2'>
@@ -340,7 +342,9 @@ export default function CreateListing() {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
-                  <span className='text-xs'>($ / month)</span>
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                 </div>
               </div>
             )}
