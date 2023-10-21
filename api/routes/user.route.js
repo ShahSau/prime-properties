b49d88bb6561e9e793b2e669bc7bc91618707746
shahriar <shahriarksaurov@gmail.com>
2023-10-21 21:35:20 +0300

    contact landlord

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { updateUser, deleteUser, getUserListings } from '../controllers/user.controller.js'
+import { updateUser, deleteUser, getUserListings, getUser } from '../controllers/user.controller.js'
 import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router()
@@ -12,5 +12,6 @@ router.post('/update/:id', verifyToken, updateUser)
 
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 export default router
