0abe60c788ac7cb13cea630c0352b5396bbf40eb
shahriar <shahriarksaurov@gmail.com>
2023-11-15 19:59:49 +0600

    pre install

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRouter from './routes/auth.route.js'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import listingRouter from './routes/listing.route.js'
-
+import path from 'path'
 dotenv.config()
 
 const app = express()
@@ -20,6 +20,9 @@ mongoose
     console.log(err)
   })
 
+const __dirname = path.resolve()
+
+
 app.use(cookieParser())
 //app.use(express.json());
 app.listen(3000, () => {
@@ -29,6 +32,12 @@ app.use(bodyParser.json())
 app.use('/api/user', useRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
